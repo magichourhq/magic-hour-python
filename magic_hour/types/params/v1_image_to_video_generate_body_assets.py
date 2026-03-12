700988d594a51ec93080cb8c48eb4dc6300dd60c
David Hu <davidhu314@gmail.com>
2026-03-12 14:14:29 -0700

    fix

--- magic_hour/types/params/v1_image_to_video_generate_body_assets.py
+++ magic_hour/types/params/v1_image_to_video_generate_body_assets.py
@@ -7,6 +7,23 @@ class V1ImageToVideoGenerateBodyAssets(typing_extensions.TypedDict):
     Provide the assets for image-to-video.
     """
 
+    end_image_file_path: typing_extensions.NotRequired[str]
+    """
+    The path of the image to use as the last frame of the video. This value is either
+    - a direct URL to the image file
+    - a path to a local file
+
+    * **`ltx-2`**: Not supported
+    * **`seedance`**: Supports 480p, 720p, 1080p.
+    * **`kling-2.5`**: Supports 1080p.
+    * **`kling-3.0`**: Supports 1080p.
+    * **`sora-2`**: Not supported
+    * **`veo3.1`**: Not supported
+    
+    Legacy models:
+    * **`kling-1.6`**: Not supported
+    """
+
     image_file_path: typing_extensions.Required[str]
     """
     The path of the image file. This value is either
